921621e356a331a086905d78bec9ec85aaa83307
Olexandr-Shyshko <olexandr.shishko@gmail.com>
2023-01-24 21:44:18 +0200

    even numbers

--- src/com/ua/homework05/Main.java
+++ src/com/ua/homework05/Main.java
@@ -2,8 +2,10 @@ package com.ua.homework05;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 6;
+        int a = 4;
         int b = 5;
+        boolean aEven = (a % 2 == 0);
+        boolean bEven = (b % 2 == 0);
         int max, min;
         if (a > b) {
             max = a;
@@ -19,6 +21,18 @@ public class Main {
             System.out.println("Більше число: "+max);
             System.out.println("Менше число:  "+min);
         }
+        if (aEven) {
+            System.out.println("Число а парне");
+        }
+            else {
+            System.out.println("Число а непарне");
+            }
+        if (bEven) {
+            System.out.println("Число b парне");
+        }
+        else {
+            System.out.println("Число b непарне");
+        }
+        }
 
-    }
 }
