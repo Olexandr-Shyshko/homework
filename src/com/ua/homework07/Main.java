9fd2954722abd7d24564cb366f0131b9df9c9ecc
Olexandr-Shyshko <olexandr.shishko@gmail.com>
2023-01-31 20:02:44 +0200

    HW7: step 2

--- src/com/ua/homework07/Main.java
+++ src/com/ua/homework07/Main.java
@@ -4,12 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         int numberForCube = 5;
-        System.out.println("Cube of number "+numberForCube+" = "+main.getCubeOfNumber(numberForCube));
-
+        System.out.println("1. Cube of number "+numberForCube+" = "+main.getCubeOfNumber(numberForCube));
+        System.out.println("----------------------");
+        int countStar = 10;
+        System.out.print("2. Print countStar "+countStar+" = ");
+        main.printStar(countStar);
+        System.out.println("----------------------");
     }
 
     int getCubeOfNumber (int num){
         return num*num*num;
+    }
+
+    void printStar (int cnt){
+        for (int i = 1; i < cnt; i++) {
+            System.out.print("*");
+            }
     }
 
 }
