4965397a962163b28eaf83f83fec8ee9ae8bca30
Olexandr-Shyshko <olexandr.shishko@gmail.com>
2023-01-31 20:14:09 +0200

    HW7: step 3

--- src/com/ua/homework07/Main.java
+++ src/com/ua/homework07/Main.java
@@ -3,13 +3,17 @@ package com.ua.homework07;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        int numberForCube = 5;
+        int numberForCube = 3;
         System.out.println("1. Cube of number "+numberForCube+" = "+main.getCubeOfNumber(numberForCube));
         System.out.println("----------------------");
-        int countStar = 10;
-        System.out.print("2. Print countStar "+countStar+" = ");
+        int countStar = 5;
+        System.out.print("2. Print countStar "+countStar+" : ");
         main.printStar(countStar);
         System.out.println("----------------------");
+        int countSymbol = 4;
+        String symbol = "#";
+        System.out.print("3. Print countSymbol "+countSymbol+" : ");
+        main.printSymbol(countSymbol,symbol);
     }
 
     int getCubeOfNumber (int num){
@@ -17,9 +21,16 @@ public class Main {
     }
 
     void printStar (int cnt){
-        for (int i = 1; i < cnt; i++) {
+        for (int i = 0; i < cnt; i++) {
             System.out.print("*");
             }
+        System.out.println();
+    }
+    void printSymbol (int cnt, String str){
+        for (int i = 0; i < cnt; i++) {
+            System.out.print(str);
+        }
+        System.out.println();
     }
 
 }
