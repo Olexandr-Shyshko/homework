297c0d09c163389ff65e6c52d610bfe3a2480812
Olexandr-Shyshko <olexandr.shishko@gmail.com>
2023-03-19 23:52:33 +0200

    HomeWork18

--- src/com/ua/homework18/Test.java
+++ src/com/ua/homework18/Test.java
@@ -1,5 +1,7 @@
 package com.ua.homework18;
 
+import com.ua.homework18_1.Student;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,36 +9,36 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        Random random = new Random();
-        List<Integer> list1 = new ArrayList<>();
+    Random random = new Random();
+    List<Integer> list1 = new ArrayList();
+    int capacity1 = 10;
 
-        int capacity1 = 10;
-        for (int i = 0; i < capacity1; i++) {
-            int number = random.nextInt(50);
-            list1.add(number);
-        }
+    int capacity2;
+        for(int i = 0; i < capacity1; ++i) {
+        capacity2 = random.nextInt(50);
+        list1.add(capacity2);
+    }
+
         System.out.println(list1);
         System.out.println("---------------------------------");
+    LinkedList<Integer> list2 = new LinkedList();
+    capacity2 = 2000000;
 
-        LinkedList <Integer> list2 = new LinkedList<>();
-        int capacity2 = 2_000_000;
-        for (int i = 0; i < capacity2; i++) {
-            int number = random.nextInt(2);
-            list2.addLast(number);
-        }
+        for(int i = 0; i < capacity2; ++i) {
+        int number = random.nextInt(2);
+        list2.addLast(number);
+    }
+
         System.out.println(list2);
         System.out.println("---------------------------------");
-
-        List <Student> studentList1 = new ArrayList<>();
-        Student st1 = new Student(1,"Ira","Ivanova","UP-19");
+    List<com.ua.homework18_1.Student> studentList1 = new ArrayList();
+    com.ua.homework18_1.Student st1 = new com.ua.homework18_1.Student(1, "Ira", "Ivanova", "UP-19");
         studentList1.add(st1);
         System.out.println(st1);
         System.out.println("---------------------------------");
-
-        List <Student> studentList2 = new LinkedList<>();
-        Student st2 = new Student(2,"Ivan","Petrov","FK-17");
+    List<com.ua.homework18_1.Student> studentList2 = new LinkedList();
+    com.ua.homework18_1.Student st2 = new Student(2, "Ivan", "Petrov", "FK-17");
         studentList2.add(st2);
         System.out.println(st2);
-    }
-
+}
 }
